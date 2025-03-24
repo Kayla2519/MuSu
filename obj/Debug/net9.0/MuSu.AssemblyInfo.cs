3d5af7cb151a662e8d1cc1eab5ee1d52e44f9fdf
Kayla2519 <kayla2519@icloud.com>
2025-03-23 23:24:12 -0300

    More CSS

--- obj/Debug/net9.0/MuSu.AssemblyInfo.cs
+++ obj/Debug/net9.0/MuSu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuSu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61bb84cc57841409465eda27cdba0d52723bf7ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77bce8264525561b96494fcf061890026b63cdea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuSu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuSu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
