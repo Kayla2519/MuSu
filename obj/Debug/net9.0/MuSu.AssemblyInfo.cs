c2dbff7f97ac69cafcfa8000e23e4df16a5c4f4a
Kayla2519 <kayla2519@icloud.com>
2025-03-25 22:03:07 -0300

    I don't even know

--- obj/Debug/net9.0/MuSu.AssemblyInfo.cs
+++ obj/Debug/net9.0/MuSu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuSu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4f27d24cb14a3a481d2ad79b1d0467b6574c77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163bd9deebfd6658ed923d46236366c87250c141")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuSu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuSu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
